cc03bf0f97026a6f3a8959d1c847866f02e8a6a4
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-19 15:12:16 -0300

    🐞fix: infra-test.yml

--- infraestructure/02-acr/backend.tf
+++ infraestructure/02-acr/backend.tf
@@ -1,15 +1,9 @@
+# backend.tf (APENAS backend, SEM provider)
 terraform {
   backend "azurerm" {
     resource_group_name  = "__TF_BACKEND_RESOURCE_GROUP__"
     storage_account_name = "__TF_BACKEND_STORAGE_ACCOUNT__"
     container_name       = "__TF_BACKEND_CONTAINER__"
     key                  = "acr.tfstate"
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
   }
 }
