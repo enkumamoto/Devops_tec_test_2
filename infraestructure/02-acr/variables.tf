85518e82fd1acad65aa7aaaae3fc54be4bb5288c
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-19 15:00:05 -0300

    🐞fix: ACR

--- infraestructure/02-acr/variables.tf
+++ infraestructure/02-acr/variables.tf
@@ -27,3 +27,28 @@ variable "admin_enabled" {
   description = "Enable admin user for ACR"
   type        = bool
 }
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = ""
+}
+
+variable "azure_client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+  default     = ""
+}
+
+variable "azure_client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  default     = ""
+}
