ede1990f223eaff5e05c0c9703da3bfee4d8904d
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-19 14:39:27 -0300

    🐞fix: backend.tf

--- infraestructure/01-network/backend.tf
+++ infraestructure/01-network/backend.tf
@@ -1,19 +1,19 @@
-# Atualizar arquivo: infraestructure/01-network/backend.tf
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "__TF_BACKEND_RESOURCE_GROUP__"
-    storage_account_name = "__TF_BACKEND_STORAGE_ACCOUNT__"
-    container_name       = "__TF_BACKEND_CONTAINER__"
-    key                  = "network.tfstate"
-  }
+# # Atualizar arquivo: infraestructure/01-network/backend.tf
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "__TF_BACKEND_RESOURCE_GROUP__"
+#     storage_account_name = "__TF_BACKEND_STORAGE_ACCOUNT__"
+#     container_name       = "__TF_BACKEND_CONTAINER__"
+#     key                  = "network.tfstate"
+#   }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
 
 provider "azurerm" {
   features {}
