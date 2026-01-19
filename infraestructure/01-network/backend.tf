1781373a67c10921575bd38d7f9117928ab3d7f7
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-19 17:42:50 -0300

    🐞fix: NTW

--- infraestructure/01-network/backend.tf
+++ infraestructure/01-network/backend.tf
@@ -7,18 +7,18 @@ terraform {
     key                  = "network.tfstate"
   }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+  # required_providers {
+  #   azurerm = {
+  #     source  = "hashicorp/azurerm"
+  #     version = "~> 3.0"
+  #   }
+  # }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-}
+# provider "azurerm" {
+#   features {}
+#   subscription_id = var.azure_subscription_id
+#   client_id       = var.azure_client_id
+#   client_secret   = var.azure_client_secret
+#   tenant_id       = var.azure_tenant_id
+# }
