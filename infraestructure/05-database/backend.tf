737062d51b19ddd4be886c7c545f5989ff2a1de9
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-20 17:25:27 -0300

    ✨feat: database

--- infraestructure/05-database/backend.tf
+++ infraestructure/05-database/backend.tf
@@ -1,22 +1,22 @@
-terraform {
-  required_version = ">= 1.5.0"
+# terraform {
+#   required_version = ">= 1.5.0"
 
-  backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = "database.tfstate"
-    # use_azuread_auth     = false  # Remova se presente
-  }
+#   backend "azurerm" {
+#     resource_group_name  = ""
+#     storage_account_name = ""
+#     container_name       = ""
+#     key                  = "database.tfstate"
+#     # use_azuread_auth     = false  # Remova se presente
+#   }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
