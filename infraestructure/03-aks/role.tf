9bc46f5a919f43e72cbb684fdc57f3be4b570775
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-21 18:51:49 -0300

    🐞fix: AKS ACR INFRA.YML

--- infraestructure/03-aks/role.tf
+++ infraestructure/03-aks/role.tf
@@ -5,7 +5,17 @@
 #   principal_id = azurerm_kubernetes_cluster.devops.kubelet_identity[0].object_id
 # }
 resource "azurerm_role_assignment" "aks_acr" {
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id = azurerm_kubernetes_cluster.devops.kubelet_identity[0].object_id
+
   role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
+  scope                = data.terraform_remote_state.acr.outputs.acr_id
+  count                = data.terraform_remote_state.acr.outputs.acr_id != "" ? 1 : 0
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  count = var.enable_acr_rbac && data.terraform_remote_state.acr.outputs.acr_id != "" ? 1 : 0
+
+  scope                = data.terraform_remote_state.acr.outputs.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.devops.kubelet_identity[0].object_id
 }
