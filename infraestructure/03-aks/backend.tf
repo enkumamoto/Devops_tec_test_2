bb45387b73f7a423a362e368e4b4fa7514ce6bb8
enkumamoto <eiji.kumamoto@gmail.com>
2026-01-22 10:46:39 -0300

    🐞fix: aks

--- infraestructure/03-aks/backend.tf
+++ infraestructure/03-aks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    # resource_group_name  = "__TF_BACKEND_RESOURCE_GROUP__"
-    # storage_account_name = "__TF_BACKEND_STORAGE_ACCOUNT__"
-    # container_name       = "__TF_BACKEND_CONTAINER__"
-    key = "aks.tfstate"
+    resource_group_name  = "__TF_BACKEND_RESOURCE_GROUP__"
+    storage_account_name = "__TF_BACKEND_STORAGE_ACCOUNT__"
+    container_name       = "__TF_BACKEND_CONTAINER__"
+    key                  = "aks.tfstate"
   }
 }
 
